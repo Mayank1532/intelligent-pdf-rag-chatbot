02813a8cb470cb7da5fd6c4f2a745509933cced1
Mayank Dhillon <dhillonmayank2000@gmail.com>
2026-08-10 19:13:58 +0530

    add vector store

---
+++ src/vector_store.py
@@ -0,0 +1,9 @@
+from langchain_chroma import Chroma
+
+
+def create_vector_store(documents, embeddings):
+    return Chroma.from_documents(
+        documents=documents,
+        embedding=embeddings,
+        persist_directory="data/chroma",
+    )
